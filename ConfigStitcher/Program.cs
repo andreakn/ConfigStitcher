482b7a8335710834affafad5d6ae4117d63f1563
Andreas Knudsen <andreas.knudsen@gmail.com>
2015-07-23 15:45:30 +0200

    some changes

--- ConfigStitcher/Program.cs
+++ ConfigStitcher/Program.cs
@@ -9,13 +9,18 @@ namespace ConfigStitcher
 {
    public class Program
    {
-      static void Main(string[] args)
+      public static void Main(string[] args)
       {
          var arguments = new Arguments(args);
          //Console.WriteLine("currentdir " + Environment.CurrentDirectory);
          //Console.WriteLine("AppDomain.CurrentDomain.BaseDirectory " + );
          //Console.WriteLine("Assembly.GetEntryAssembly().Location " + Assembly.GetEntryAssembly().Location);
-         
+
+         if (arguments["help"] == "true"|| arguments["?"] == "true")
+         {
+            PrintHelp();
+            return;
+         }
 
 
 
@@ -36,6 +41,25 @@ namespace ConfigStitcher
 
          var stitcher = new Stitcher();
          stitcher.PerformConfigStitching(workingDir,recipeLocation, enableTrace);
+
+      }
+
+      public static void PrintHelp()
+      {
+         Console.WriteLine(@"Usage:
+ConfigStitcher [-workingdir ""<path>""] [-recipe ""<path>""] [-trace <true|false>] 
+
+Arguments:
+- recipe: relative or absolute path to recipe file
+- workingdir: root path for resolving the recipe if relative path is used for recipe
+- trace: whether to write diagnostic info into the output file(s)  (NB: this makes the files unusable by .net, but can be handy to verify whence a setting came from)
+
+Defaults: 
+- recipe: ""recipe.txt""
+- workingdir: directory path of ConfigStitcher.exe 
+- trace: false
+
+");
 
       }
    }
